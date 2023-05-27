c16484924c3d3b49850c2c8557a092bd8fb2d18a
doheuncho <dh30594@gmail.com>
2023-05-27 17:43:51 +0900

    :sparkles: Add update and delete function in feed

--- content/urls.py
+++ content/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import Main, UploadFeed, Profile, UploadReply, ToggleBookmark
+from .views import Main, UploadFeed, UpdateFeed, DeleteFeed, Profile, UploadReply, ToggleBookmark
 
 
 urlpatterns = [
     path('upload', UploadFeed.as_view()),
+    path('update', UpdateFeed.as_view()),
+    path('delete', DeleteFeed.as_view()),
     path('reply', UploadReply.as_view()),
     path('bookmark', ToggleBookmark.as_view()),
     path('profile', Profile.as_view()),
